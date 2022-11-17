111390beffced410f6ca98db0d7055d352de27cc
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2022-11-17 18:18:16 -0300

    feat: relatorio para o insert_sort

---
+++ report/insert_sort/dead_mutants/strongly_too/mutant3.py
@@ -0,0 +1,29 @@
+'''
+Created on 2014.9.1
+
+@author: Garvin Li
+'''
+
+import sys
+
+def insert_sort(item1,item2,item3,item4,item5):
+    list=[item1,item2,item3,item4,item5]
+   
+    for i in range(1,len(list)):
+        key=list[i]
+        j=i-1
+        while (j <= 0 and key < list[j]): #PM
+            list[j+1]=list[j]
+            j-=1
+        list[j+1]=key
+    return list           
+
+
+if __name__ == '__main__':
+    item1 = float(sys.argv[1:][0])
+    item2 = float(sys.argv[1:][1])
+    item3 = float(sys.argv[1:][2])
+    item4 = float(sys.argv[1:][3])
+    item5 = float(sys.argv[1:][4])
+    print(insert_sort(item1,item2,item3,item4,item5))
+    
