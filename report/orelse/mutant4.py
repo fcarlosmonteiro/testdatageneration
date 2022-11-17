f6ab83b7c6ceff3dab4ad67ad7be9662fe0cea02
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2022-11-17 15:22:52 -0300

    feat: relatorio para o orelse

---
+++ report/orelse/mutant4.py
@@ -0,0 +1,11 @@
+def func1(a, b, c):
+    if a >= 10: #PM
+        for i in range(10):
+            b += 1
+
+    else:
+        if b > 10:
+            for i in range(5):
+                c += 1
+
+    return a, b, c
