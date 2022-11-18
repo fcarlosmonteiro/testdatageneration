d8496903a171ef6355c2e7741c67be85aec3aaac
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2022-11-18 14:13:58 -0300

    feat: relatorio para o uri 1132

---
+++ report/uri_1132/mutant7.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def func(x, y):
+    z = 0
+
+    if x > y:
+        z = x
+        x = y
+        y = z
+
+    s = 0
+
+    while y <= x: #PM
+        if x % 13 != 0:
+            s += x
+        x+=1
+        
+    return s
+
+if __name__ == '__main__':
+    x = float(sys.argv[1:][0])
+    y = float(sys.argv[1:][1])
+    print(func(x, y))
