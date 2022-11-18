8669adf72f09ff9ce01644bc37b152f2c07160d6
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2022-11-18 18:18:49 -0300

    feat: relatorio para o uri 2702

---
+++ target/uri_2702.py
@@ -0,0 +1,26 @@
+import sys
+
+def func(item1,item2,item3,item4,item5,item6):
+    a,b,c,d,e,f = [float(item1),float(item2),float(item3),float(item4),float(item5),float(item6)]
+    r = 0
+
+    if int(a) < int(d):
+        r += int(d) - int(a)
+
+    if int(b) < int(e):
+        r += int(e) - int(b)
+
+    if int(c) < int(f):
+        r += int(f) - int(c)
+
+    return r
+
+
+if __name__ == "__main__":
+    item1 = float(sys.argv[1:][0])
+    item2 = float(sys.argv[1:][1])
+    item3 = float(sys.argv[1:][2])
+    item4 = float(sys.argv[1:][3])
+    item5 = float(sys.argv[1:][4])
+    item6 = float(sys.argv[1:][5])
+    print(func(item1,item2,item3,item4,item5,item6))
