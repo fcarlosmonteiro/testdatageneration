cf8d266f9d40226829a9cad46324f06f9bd9bc65
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2022-11-18 11:32:17 -0300

    feat: valores que mataram os pontos no insert_sort

--- report/insert_sort/dead_mutants/mutant6.py
+++ report/insert_sort/dead_mutants/mutant6.py
@@ -12,7 +12,7 @@ def insert_sort(item1,item2,item3,item4,item5):
     for i in range(1,len(list)):
         key=list[i]
         j=i-1
-        while (j >= 0 and key <= list[j]): #PM
+        while (j >= 0 and key <= list[j]): #PM | type_kill=weakly args=[2038.07, -2657.29, 5792.23, -7785.11, -6426.6]
             list[j+1]=list[j]
             j-=1
         list[j+1]=key
