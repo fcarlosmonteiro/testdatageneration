f148f94540969a76fccb65fdae01c346800d1435
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2022-11-19 14:57:05 -0300

    feat: relatorio para o type triangle

---
+++ report/type_triangle/mutants_with_logic_error/mutant9.py
@@ -0,0 +1,20 @@
+import sys
+
+def type_triangle(x, y, z):
+    x = int(float(x))
+    y = int(float(y))
+    z = int(float(z))
+
+    if x == y == z:
+        return "Equilateral triangle"
+    elif x==y or y==z or z==x:
+        return "isosceles triangle"
+    else:
+        return "Scalene triangle"
+
+
+if __name__ != '__main__': #PM
+    x = sys.argv[1:][0]
+    y = sys.argv[1:][1]
+    z = sys.argv[1:][2]
+    print(type_triangle(x, y, z))
