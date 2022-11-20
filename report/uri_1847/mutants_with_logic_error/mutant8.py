45a4d07b8278a1ff81edb2b5f2084b81f679c3c8
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2022-11-20 14:12:27 -0300

    feat: relatorio para o uri 1847

---
+++ report/uri_1847/mutants_with_logic_error/mutant8.py
@@ -0,0 +1,30 @@
+import sys
+
+def func(x, y, z):
+    x = int(float(x))
+    y = int(float(y))
+    z = int(float(z))
+
+    if x > y and z >= y:
+        return ':)'
+    elif y > x and y >= z:
+        return ':('
+    elif y > x and z > y and (z - y) < (y - x):
+        return ':('
+    elif y > x and z > y and (z - y) >= (y - x):
+        return ':)'
+    elif x > y and y > z and (y - z) < (x - y):
+        return ':)'
+    elif y > x and y > z and (y - z) >= (y - z):
+        return ':('
+    elif x == y and z > y:
+        return ':)'
+    else:
+        return ':('
+
+
+if not (__name__ == '__main__'): #PM
+    x = sys.argv[1:][0]
+    y = sys.argv[1:][1]
+    z = sys.argv[1:][2]
+    print(func(x,y,z))
