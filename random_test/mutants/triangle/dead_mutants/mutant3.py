4a4424e23f9a8126527d2ac2723c5312399864c0
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2022-11-21 11:50:51 -0300

    feat: relatorio para o teste de dado random para o triangle

---
+++ random_test/mutants/triangle/dead_mutants/mutant3.py
@@ -0,0 +1,35 @@
+import sys
+
+def triangle(a, b, c):
+    longest = 0
+    m = 0
+    n = 0
+
+    if (a > b):
+        if (a > c):
+            longest = a
+            m, n = b, c
+
+        else:
+            longest = c
+            m, n = a, b
+
+    else:
+    elif not (b > c): #PM | type_kill=random args=[1568.7453996383738, 9318.954833273026, 8868.572944242089]
+            longest = b
+            m, n = a, c
+        else:
+            longest = c
+            m, n = a, b
+
+    if longest < (m + n):
+        return True
+
+    return False
+
+
+if __name__ == "__main__":
+    a = float(sys.argv[1:][0])
+    b = float(sys.argv[1:][1])
+    c = float(sys.argv[1:][2])
+    print(triangle(a, b, c))
