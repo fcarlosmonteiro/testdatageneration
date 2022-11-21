27828081a9287d3d6641bc91a8ab7bfd3f782dcc
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2022-11-21 13:50:23 -0300

    feat: relatorio para o teste de dado random para o uri_2484

---
+++ random_test/mutants/uri_2484/dead_mutants/mutant1.py
@@ -0,0 +1,29 @@
+import sys
+
+def func(x):
+    texto = str(x)
+    i = 0
+    result = ""
+    for nIndex in range(len(texto)):
+        line = ""
+        index = 0
+
+        for i2 in range(i):
+            line += " "
+
+        for caracter in texto:
+            line += caracter
+
+            if not (index != len(texto) - 1): #PM | type_kill=weakly args=[2206.37] | type_kill=strongly args=[2206.37] | type_kill=random args=[-9205.178025443158]
+                line += " "
+            
+            index += 1
+        
+        result += "{}\n".format(line)
+        texto = texto[0:len(texto)-1]
+        i += 1
+    return result
+
+if __name__ == '__main__':
+    x = sys.argv[1:][0]
+    print(func(x))
