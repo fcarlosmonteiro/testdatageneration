0f54a25945f662e714891bf9a10cce38a106a4da
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2022-11-21 13:43:52 -0300

    feat: relatorio para o teste de dado random para o uri_2140

---
+++ random_test/mutants/uri_2140/dead_mutants/mutant8.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def func(x, y):
+    x = int(float(x))
+    y = int(float(y))
+
+    t = y - x
+    p = False
+    n = [2, 5, 10, 20, 50, 100]
+
+    for e in n:
+        for e2 in n:
+            if e + e2 == t:
+                p = True
+                break
+        if p == True:
+            break
+
+    if p == True:
+        return "possible"
+    else:
+        return "impossible"
+
+if __name__ != '__main__': #PM | type_kill=random args=[874.2551440435691, 8898.451514665983]
+    x = sys.argv[1:][0]
+    y = sys.argv[1:][1]
+    print(func(x, y))
